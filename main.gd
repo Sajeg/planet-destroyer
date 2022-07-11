9bf22dc658daeb2c089d60546f35241a185b8bd0
Sajeg <jfx@posteo.de>
2022-07-11 16:02:09 +0200

    added a bit more fun

--- main.gd
+++ main.gd
@@ -1,30 +1,65 @@
 extends Node2D
 
+var CircleColor
+var points = 1000
+var CircleNumber = 0
+
 func _ready():
 	spawn_circle()
 
 func _process(_delta):
-	if Input.is_action_pressed("1"):
-		print("1")
-		$Red.visible = true
-		$yellow.visible = false
-		$green.visible = false
-	elif Input.is_action_pressed("2"):
-		print("2")
-		$Red.visible = false
-		$yellow.visible = true
-		$green.visible = false
-	elif Input.is_action_pressed("3"):
-		print("3")
-		$Red.visible = false
-		$yellow.visible = false
-		$green.visible = true
+	add_points()
+	$Label.text = "Points: " + str(points)
 	
+
+func add_points():
+	if CircleColor == "Red":
+		if Input.is_action_pressed("1"):
+			points += 1000
+			spawn_circle()
+		elif Input.is_action_pressed("2"):
+			points -= 1000
+		elif Input.is_action_pressed("3"):
+			points -= 1000
+		else:
+			points -= 1
+	elif CircleColor == "Yellow":
+		if Input.is_action_pressed("2"):
+			points += 1000
+			spawn_circle()
+		elif Input.is_action_pressed("2"):
+			points -= 1000
+		elif Input.is_action_pressed("3"):
+			points -= 1000
+		else:
+			points -= 1
+	elif CircleColor == "Green":
+		if Input.is_action_pressed("3"):
+			points += 1000
+			spawn_circle()
+		elif Input.is_action_pressed("2"):
+			points -= 1000
+		elif Input.is_action_pressed("3"):
+			points -= 1000
+		else:
+			points -= 1
+
+
+
 func spawn_circle():
 	var rand = randi()%3
 	if rand == 0:
-		pass
+		CircleColor = "Red"
+		$RedObject.visible = true
+		$yellowObject.visible = false
+		$greenObject.visible = false
 	elif rand == 1:
-		pass
+		CircleColor = "Yellow"
+		$RedObject.visible = false
+		$yellowObject.visible = true
+		$greenObject.visible = false
 	elif rand == 2:
-		pass
+		CircleColor = "Green"
+		$RedObject.visible = false
+		$yellowObject.visible = false
+		$greenObject.visible = true
